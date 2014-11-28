e1d2055dbce1420d803b9ce0094f894ba4c72122
kap0ton <kap_ton@me.com>
2014-11-28 21:34:26 +1300

    Initial project created

---
+++ CinemaparkSolution/Cinemapark/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Cinemapark.Resources;
+
+namespace Cinemapark
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
